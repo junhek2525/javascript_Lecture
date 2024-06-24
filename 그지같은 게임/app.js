7cfe2250486e41731c47a953c7e1970c70f0e2d1
junhek2525 <junhek2525@gmail.com>
2024-06-24 16:25:52 +0900

    4

--- 그지같은 게임/app.js
+++ 그지같은 게임/app.js
@@ -56,10 +56,21 @@ gameInterval = setInterval(moveInvader, 1000);
 interval = setInterval(moveInvader, 1000);
 
 function gameStart(){
-    displayStatus();
+    stage[playerLoc].classList.remove("player");
+    invadersLoc.forEach(function(invader){
+        stage[invader].classList.remove("invader");
+    });
+    display.innerText = ""
+    playerLoc = stageSize*(stageSize-2)+7
+    invadersLoc = [
+        0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39
+    ]
+   makePlayer();
+   makeInvader();
+   displayStatus();
     gameInterval = setInterval(moveInvader, 1000);
     document.addEventListener("keyup", movePlayer);
-    gameRun();
+    gameRun()
 }
 function gameStop(){
     clearInterval(gameInterval);
@@ -72,16 +83,43 @@ function displayStatus(){
     display.innerText = invadersLoc.length + "/" + invadersLoc.length
 }
 
-function gameStart(){
-    stage[playerLoc].classList.remove("player");
-    invadersLoc.forEach(function(invader){
-        stage[invader].classList.remove("invader");
-    });
-    playerLoc = stageSize*(stageSize-2)+7
-    invadersLoc = [
-        0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39
-    ]
+// function gameStart(){
+    
+// }
+
+let bulletLoc = playerLoc;
+function moveBullet(){
+    stage[bulletLoc].classList.remove("bullet");
+    bulletLoc -= stageSize;
+    stage[bulletLoc].classList.add("bullet");
 }
+function shoot(e){
+    let id;
+    let bulletLoc = playerLoc;
+    function moveBullet(){
+        stage[bulletLoc].classList.remove("bullet");
+        bulletLoc -= stageSize;
+        if (bulletLoc < 0){
+            clearInterval(id);
+            return;
+        }
+        stage[bulletLoc].classList.add("bullet")
+        if (stage[bulletLoc].classList.contains("invader")){
+            stage[bulletLoc].classList.remove("invader")
+            stage[bulletLoc].classList.remove("bullet")
+            stage[bulletLoc].classList.add("boom")
+            
+        }
+    }
+    if(e.key === "ArrowUp"){
+        id = setInterval(moveBullet, 300)
+    }
+}
+
+// bulletId = setInterval(moveBullet,500);
+document.addEventListener("keydown", shoot);
+
+document.addEventListener("keyup", movePlayer);
 
 startBtn.addEventListener("click", gameStart);
 startBtn.addEventListener("click", gameStop);
